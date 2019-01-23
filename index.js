e2d41be533801e75af72f01f526099c1e66197d2
Bill Barrington <bill.barrington@candidpartners.com>
2019-01-23 16:18:37 -0500

    Move to a Promise return from Twitter.get.

--- index.js
+++ index.js
@@ -12,31 +12,35 @@ const Twitter = require('twitter');
 const consumerKey = process.env.TWITTER_CONSUMER_KEY;
 const consumerSecret = process.env.TWITTER_CONSUMER_SECRET;
 
-// auth.bearerToken(consumerKey, consumerSecret, (err, data) => {
-//     if (err) console.error(err);
-//     else console.log(Buffer.from(data).toString());
-// });
+// Returns a Promise resolving to a Twitter object on success
+const twitterClient = (consumerKey, consumerSecret) => {
+    return new Promise((resolve, reject) => {
 
-// Returns a Twitter object on success
-const twitterClient = (consumerKey, consumerSecret, callback) => {
-    // Get a bearer token first
-    const bearerToken = auth.bearerToken(consumerKey, consumerSecret, (err, bearerToken) => {
-        if (err) callback(err); else {
-            callback(null, new Twitter({
-                consumer_key: consumerKey,
-                consumer_secret: consumerSecret,
-                bearer_token: bearerToken
-            }))
-        }
+        // Get a bearer token first
+        auth.bearerToken(consumerKey, consumerSecret, (err, bearerToken) => {
+
+            if (err) reject(err); else {
+                resolve(new Twitter({
+                    consumer_key: consumerKey,
+                    consumer_secret: consumerSecret,
+                    bearer_token: bearerToken
+                }))
+            }
+        })
     })
 };
 
-twitterClient(consumerKey, consumerSecret, (err, client) => {
-    if (err) console.error(err); else {
-        // Make a query
-        
-    }
-});
+twitterClient(consumerKey, consumerSecret).then(client => {
+    client.get('https://api.twitter.com/1.1/search/tweets.json', {
+        q: '@realDonaldTrump',
+        result_type: 'recent'
+    }, function(error, tweets, response) {
+        if(error) console.error(error); else {
+            console.log(tweets);
+            console.log(response);
+        }
+    })
+}).catch(err => console.error(err));
 
 
 
