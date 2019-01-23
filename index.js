b0a6a0231d0c1c52f508c60e1bcbb6452cb7b632
Bill Barrington <bill.barrington@candidpartners.com>
2019-01-23 14:26:11 -0500

    Checkpoint.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // Lay the groundwork for application-only request to Twitter
 // todo Add TLS/SSL best practices discussed in Twitter Docs
+// todo Validate return value from 'bearerToken' call
+
 
 'use strict';
 
+// todo How does this work in production?
+require('dotenv').config();
+
 const auth = require('./lib/auth');
 
+const consumerKey = process.env.TWITTER_CONSUMER_KEY;
+const consumerSecret = process.env.TWITTER_CONSUMER_SECRET;
 
-auth.bearerToken(consumerKey, consumerSecret, (data) => {
-    console.log(Buffer.from(data).toString());
-})
+auth.bearerToken(consumerKey, consumerSecret, (err, data) => {
+    if (err) console.error(err);
+    else console.log(Buffer.from(data).toString());
+});
