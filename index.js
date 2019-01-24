05f9214f4a725fefbd6fd051927fa1db0f829faf
Bill Barrington <bill.barrington@candidpartners.com>
2019-01-24 15:06:50 -0500

    Rewrote handler using v2.

--- index.js
+++ index.js
@@ -1,53 +1,125 @@
 'use strict';
 const Alexa = require('ask-sdk');
-const tweet = require('./lib/tweet');
-
-const APP_ID = 'amzn1.ask.skill.e6029b28-6b6f-47a0-b508-d26ab5435921';
-
-const GET_TWEET_MESSAGE = "Here's the latest tweet from the orange moron: ";
-const HELP_MESSAGE = 'You can say give me the latest tweet';
-const HELP_REPROMPT = 'What can I help you with?';
-const STOP_MESSAGE = 'Goodbye!';
+const tweets = require('./lib/tweets');
 
 
-const latestTweet = tweet.getSortedTweets().then(tweets => {
+const latestTweet = tweets.getSortedTweets().then(tweets => {
     return tweets[0];
 });
 
 
-const handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetLatestTweetIntent');
-    },
-    'GetLatestTweetIntent': function () {
-        latestTweet.then(tweet => {
-            const speechOutput = GET_TWEET_MESSAGE + tweet.full_text;
+// -----------------------------------------------------
+// Handlers...
 
-            this.response.speak(speechOutput);
-            this.emit(':responseReady');
-        });
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
-    'AMAZON.HelpIntent': function () {
-        const speechOutput = HELP_MESSAGE;
-        const reprompt = HELP_REPROMPT;
+    handle(handlerInput) {
 
-        this.response.speak(speechOutput).listen(reprompt);
-        this.emit(':responseReady');
+        const speechText = 'Welcome to the Trump Tweeter';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Trump Tweeter', speechText)
+            .getResponse();
+    }
+};
+
+const LatestTweetIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'LatestTweetIntent';
     },
-    'AMAZON.CancelIntent': function () {
-        this.response.speak(STOP_MESSAGE);
-        this.emit(':responseReady');
+    handle(handlerInput) {
+        return latestTweet.then(tweet => {
+
+            const speechText = `Hello Trump Tweet Lovers! Here's my latest tweet ${tweet}`;
+
+            return handlerInput.responseBuilder
+                .speak(speechText)
+                .withSimpleCard('Trump Tweeter', speechText)
+                .getResponse();
+        })
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
     },
-    'AMAZON.StopIntent': function () {
-        this.response.speak(STOP_MESSAGE);
-        this.emit(':responseReady');
+    handle(handlerInput) {
+        const speechText = 'Go apply for a government job!';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Trump Tweeter', speechText)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speechText = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Trump Tweeter', speechText)
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        //any cleanup logic goes here
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+
+        return handlerInput.responseBuilder
+            .speak('Sorry, I can\'t understand the command. Have you considered applying for a job at the White House?')
+            .reprompt('Sorry, I can\'t understand the command. But Mexico will pay for the wall!')
+            .getResponse();
     },
 };
 
-exports.handler = function (event, context, callback) {
-    const alexa = Alexa.handler(event, context, callback);
-    alexa.APP_ID = APP_ID;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
+let skill;
 
+exports.handler = async function (event, context) {
+    console.log(`REQUEST++++${JSON.stringify(event)}`);
+    if (!skill) {
+        skill = Alexa.SkillBuilders.custom()
+            .addRequestHandlers(
+                LaunchRequestHandler,
+                LatestTweetIntentHandler,
+                HelpIntentHandler,
+                CancelAndStopIntentHandler,
+                SessionEndedRequestHandler,
+            )
+            .addErrorHandlers(ErrorHandler)
+            .create();
+    }
+
+    const response = await skill.invoke(event, context);
+    console.log(`RESPONSE++++${JSON.stringify(response)}`);
+
+    return response;
+};
